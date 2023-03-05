00cad0f063777efeb8498fe85ed09b4cf7f1c346
Lars Bo Rasmussen <lars.bo.rasmussen@sentia.com>
2023-03-05 02:36:34 +0100

    fix

--- chatgpt.py
+++ chatgpt.py
@@ -259,6 +259,8 @@ class ChatGPT(Plugin):
             self.driver.reactions.delete_reaction(self.driver.user_id,
                                                   message.id, "thought_balloon")
             self.driver.react_to(message, "x")
+
+            return
         print(response)
         self.add_usage_for_user(message.sender_name,
                                 response['usage']['total_tokens'])

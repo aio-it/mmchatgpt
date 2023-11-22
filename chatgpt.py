375ed39423c6b5fcf6441cee05914b44d9443d88
Lars Bo Rasmussen <lars.bo.rasmussen@sentia.com>
2023-11-22 08:11:12 +0100

    quickfix

--- chatgpt.py
+++ chatgpt.py
@@ -507,6 +507,7 @@ class ChatGPT(Plugin):
                         size="1024x1024",
                         model="dall-e-3",
                         response_format="url",
+                        quality="hd",
                     )
                     # response = openai.Image.create(prompt=text, n=1, size="1024x1024")
                     image_url = response.data[0].url

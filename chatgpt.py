b79aefd6c4d558c66e19d7c833af8c8046898526
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-18 16:38:52 +0100

    coder

--- chatgpt.py
+++ chatgpt.py
@@ -1591,7 +1591,8 @@ class ChatGPT(Plugin):
                 import urllib.parse
                 domain = urllib.parse.urlparse(input).netloc
                 # call validateinput again with domain
-                if "error" not in await self.validateinput(domain,["domain"]):
+                result = await self.validateinput(domain,["domain"])
+                if "error" not in result:
                     return True
         if "asn" in types:
             if re.match(r"(AS|as)[0-9]+",input):

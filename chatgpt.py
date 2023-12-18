fde32efe774521944ed3155e3d481b89d339483b
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-18 18:22:36 +0100

    coder

--- chatgpt.py
+++ chatgpt.py
@@ -1656,7 +1656,7 @@ class ChatGPT(Plugin):
         if "string" in types:
             if re.match(r"[a-zA-Z0-9_-]+",input):
                 return True
-        return { "error": f"invalid input: {input} (no matches) for {' '.join(types)}" }
+        return { "error": f"invalid input: {input} (no matches) for types {', '.join(types)}" }
 
     @listen_to(r"^!(.*)")
     async def run_command(self,message: Message, command):

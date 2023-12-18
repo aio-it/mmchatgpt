9c3bbcaedaf94fc79569f88396f5feb16d65b437
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-18 18:33:42 +0100

    coder

--- chatgpt.py
+++ chatgpt.py
@@ -1671,7 +1671,7 @@ class ChatGPT(Plugin):
         if not self.is_user(message.sender_name):
             self.driver.reply_to(message, f"Error: {message.sender_name} is not a user")
             return
-        await self.log(f"{message.sender_name} tried to run command: {command}")
+        await self.log(f"{message.sender_name} tried to run command: !{command}")
         # split command into command and input
         command = command.split(" ", 1)
         if len(command) == 1:

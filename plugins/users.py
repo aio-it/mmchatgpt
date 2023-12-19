4ce2c951c0d4954b805912575e62052f7d3dd981
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-20 00:50:53 +0100

    coder

--- plugins/users.py
+++ plugins/users.py
@@ -18,9 +18,8 @@ NEEDWHITELIST = False  # if true only users in the users can use the bot
 class Users(Plugin):
     """manage users"""
     def __init__(self, driver: Driver = None, plugin_manager: PluginManager = None, settings: Settings = None):
-        self.driver = driver
         if (self.driver is not None) and (plugin_manager is not None) and (settings is not None):
-            self.initialize(self.driver, plugin_manager, settings)
+            self.initialize(driver, plugin_manager, settings)
     def initialize(
         self,
         driver: Driver,

1d9552261f80d8af1e23f44d3e4c9b68945f825d
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-20 00:14:44 +0100

    coder

--- plugins/users.py
+++ plugins/users.py
@@ -30,7 +30,7 @@ class Users(Plugin):
         self.settings = settings
         self.plugin_manager = plugin_manager
         self.helper = Helper(self.driver)
-        self.redis = self.helper.redis
+        self.redis = self.helper.get_redis()
         if self.redis.scard("admins") <= 0 and len(ADMINS) > 0:
             self.redis.sadd("admins", *ADMINS)
         if self.redis.scard("users") <= 0 and len(USERS) > 0:

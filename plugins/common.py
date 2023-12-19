f742f64d4a481495aa70b597389680c394f869e6
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-19 23:37:01 +0100

    coder

--- plugins/common.py
+++ plugins/common.py
@@ -76,7 +76,7 @@ class Users(Plugin):
         # check if user is banned
         if self.redis.exists(f"ban:{self.u2id(username)}"):
             return False
-        if NEEDWHITELIST == False
+        if NEEDWHITELIST == False:
             return True
         return True if self.u2id(username) in self.redis.smembers("users") else False
     def is_admin(self, username):

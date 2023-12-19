37a7f8182ec7364851b043f838e8f3b7ec081ce5
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-20 00:35:42 +0100

    coder

--- plugins/tts.py
+++ plugins/tts.py
@@ -22,7 +22,7 @@ class TTS(Plugin):
       self.settings = settings
       self.plugin_manager = plugin_manager
       self.helper = Helper(self.driver)
-      self.users = self.plugin_manager.plugins["Users"]
+      self.users = self.plugin_manager.plugins
 
     @listen_to(r"^\.drtts ([\s\S]*)")
     async def drtts(self, message: Message, text: str):

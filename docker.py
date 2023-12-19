4f985e23a3fde9d4f48cfba886e3398b79a0afac
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-19 15:40:16 +0100

    coder

--- docker.py
+++ docker.py
@@ -41,6 +41,7 @@ class Docker(Plugin):
   async def dockerps(self, message: Message):
     """list docker containers"""
     containers = await self.dockerclient.containers.list()
+    self.driver.reply_to(message,f"```{containers}```")
     for container in containers:
       self.driver.reply_to(message,f"```{container}```")
 

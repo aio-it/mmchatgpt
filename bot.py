c46e009ac58d4c62ca064852adb2d0c282da49e6
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-20 00:09:40 +0100

    coder

--- bot.py
+++ bot.py
@@ -14,22 +14,22 @@ log_channel = env.str("MM_BOT_LOG_CHANNEL")
 openai_api_key = env.str("OPENAI_API_KEY")
 giphy_api_key=env.str("GIPHY_API_KEY") or None
 bot = Bot(
-  settings=Settings(
-    MATTERMOST_URL=env.str("MM_URL"),
-    MATTERMOST_PORT=env.int("MM_PORT", 443),
-    MATTERMOST_API_PATH=env.str("MM_API_PATH", "/api/v4"),
-    BOT_TOKEN=env.str("MM_BOT_TOKEN"),
-    BOT_TEAM=env.str("MM_BOT_TEAM"),
-    SSL_VERIFY=env.bool("MM_SSL_VERIFY", True),
-  ),  # Either specify your settings here or as environment variables.
-  # Add your own plugins here.
-  plugins=[
-    ChatGPT(openai_api_key, giphy_api_key=giphy_api_key),
-    Docker(),
-    Pushups(),
-    Users(),
-    TTS(),
-    ShellCmds(),
-  ]
+    settings=Settings(
+        MATTERMOST_URL=env.str("MM_URL"),
+        MATTERMOST_PORT=env.int("MM_PORT", 443),
+        MATTERMOST_API_PATH=env.str("MM_API_PATH", "/api/v4"),
+        BOT_TOKEN=env.str("MM_BOT_TOKEN"),
+        BOT_TEAM=env.str("MM_BOT_TEAM"),
+        SSL_VERIFY=env.bool("MM_SSL_VERIFY", True),
+    ),  # Either specify your settings here or as environment variables.
+    # Add your own plugins here.
+    plugins=[
+       ChatGPT(openai_api_key, giphy_api_key=giphy_api_key),
+        Docker(),
+        Pushups(),
+        Users(),
+        TTS(),
+        ShellCmds(),
+    ]
 )
 bot.run()

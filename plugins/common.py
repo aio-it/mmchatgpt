e58b5b41c4b7bc180133639d67ead42b7756d626
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-19 22:44:27 +0100

    coder

--- plugins/common.py
+++ plugins/common.py
@@ -121,6 +121,93 @@ class Users:
             self.redis.set(f"uid:{username}", uid, ex=60 * 60)
         return uid
     
+        @listen_to(r"\.uid ([a-zA-Z0-9_-]+)")
+    async def uid(self, message: Message, username: str):
+        """get user id from username"""
+        if self.users.is_admin(message.sender_name):
+            self.driver.reply_to(message, self.get_uid(username))
+
+    def get_uid(self, username, force=False):
+        """get uid from username"""
+        # check if uid is cached in redis
+        if not force and self.redis.exists(f"uid:{username}"):
+            return self.redis.get(f"uid:{username}")
+        try:
+            uid = self.get_user_by_username(username)["id"]
+        except:
+            # uid not found
+            uid = None
+            # throw exception if user is not found
+            raise Exception(f"User not found: {username}")
+        # cache the uid in redis for 1 hour
+        if uid != None:
+            self.redis.set(f"uid:{username}", uid, ex=60 * 60)
+        return uid
+
+    def u2id(self, username):
+        """convert username to uid"""
+        return self.get_uid(username)
+
+    def id2u(self, user_id):
+        """convert uid to username"""
+        return self.get_user_by_user_id(user_id)["username"]
+
+    def check_if_username_or_id(self, username_or_id):
+        """check if username or id"""
+        try:
+            user = self.get_user_by_username(username_or_id)["username"]
+        except:
+            user = None
+        try:
+            uid = self.get_user_by_user_id(username_or_id)["id"]
+        except:
+            uid = None
+
+        if user is None and uid is None:
+            return "not found"
+        if user is not None:
+            return "user"
+        if uid is not None:
+            return "uid"
+    def user_exists(self, username):
+        """check if user exists"""
+        if self.check_if_username_or_id(username) == "not found":
+            return False
+        return True
+    def get_user_by_username(self, username):
+        """get user from username"""
+        # check if user is cached in redis
+        if self.redis.exists(f"user:{username}"):
+            return self.helper.redis_deserialize_json(self.redis.get(f"user:{username}"))
+        users = self.driver.users.get_users_by_usernames([username])
+        if len(users) == 1:
+            # cache the user in redis for 1 hour
+            self.redis.set(
+                f"user:{username}", self.helper.redis_serialize_json(users[0]), ex=60 * 60
+            )
+            return users[0]
+        if len(users) > 1:
+            # throw exception if more than one user is found
+            raise Exception(
+                f"More than one user found: {users} this is undefined behavior"
+            )
+        return None
+
+    def get_user_by_user_id(self, user_id):
+        """get user id from user_id"""
+        # check if user is cached in redis
+        if self.redis.exists(f"user:{user_id}"):
+            return self.helper.redis_deserialize_json(self.redis.get(f"user:{user_id}"))
+        try:
+            user = self.driver.users.get_user(user_id)
+            # cache the user in redis for 1 hour
+            self.redis.set(
+                f"user:{user_id}", self.helper.redis_serialize_json(user), ex=60 * 60
+            )
+            return user
+        except:
+            return None
+
     @listen_to(r"^\.banlist")
     async def banlist(self, message: Message):
         """list banned users"""

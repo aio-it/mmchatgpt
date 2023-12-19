9c540622a5d01de47b2d77229e292f2c291f616a
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-19 23:51:13 +0100

    coder

--- plugins/tts.py
+++ plugins/tts.py
@@ -20,72 +20,72 @@ class TTS(Plugin):
     self.helper = Helper(self.driver)
     self.users = Users()
 
-    @listen_to(r"^\.drtts ([\s\S]*)")
-    async def drtts(self, message: Message, text: str):
-        """use the dr tts website to get an audio clip from text"""
+  @listen_to(r"^\.drtts ([\s\S]*)")
+  async def drtts(self, message: Message, text: str):
+      """use the dr tts website to get an audio clip from text"""
 
-        if self.users.is_user(message.sender_name):
-            try:
-                with RateLimit(
-                    resource="drtts",
-                    client=message.sender_name,
-                    max_requests=1,
-                    expire=5,
-                ):
-                    # get the audio from dr tts website https://www.dr.dk/tjenester/tts?text=<text> using the requests module urlencode the text
-                    self.helper.add_reaction(message, "speaking_head_in_silhouette")
-                    # replace newlines with spaces
-                    text = text.replace("\n", " ")
-                    urlencoded_text = self.urlencode_text(text)
-                    audio_url = (
-                        f"https://www.dr.dk/tjenester/tts?text={urlencoded_text}"
-                    )
-                    # download the audio using the url
-                    filename = self.download_file_to_tmp(audio_url, "mp3")
-                    # format the link in mattermost markdown
-                    msg_txt = f"link: [drtts]({audio_url})"
-                    self.helper.remove_reaction(message, "speaking_head_in_silhouette")
-                    self.driver.reply_to(message, msg_txt, file_paths=[filename])
-                    # delete the audio file
-                    self.delete_downloaded_file(filename)
-                    await self.helper.log(f"{message.sender_name} used .drtts")
+      if self.users.is_user(message.sender_name):
+          try:
+              with RateLimit(
+                  resource="drtts",
+                  client=message.sender_name,
+                  max_requests=1,
+                  expire=5,
+              ):
+                  # get the audio from dr tts website https://www.dr.dk/tjenester/tts?text=<text> using the requests module urlencode the text
+                  self.helper.add_reaction(message, "speaking_head_in_silhouette")
+                  # replace newlines with spaces
+                  text = text.replace("\n", " ")
+                  urlencoded_text = self.urlencode_text(text)
+                  audio_url = (
+                      f"https://www.dr.dk/tjenester/tts?text={urlencoded_text}"
+                  )
+                  # download the audio using the url
+                  filename = self.download_file_to_tmp(audio_url, "mp3")
+                  # format the link in mattermost markdown
+                  msg_txt = f"link: [drtts]({audio_url})"
+                  self.helper.remove_reaction(message, "speaking_head_in_silhouette")
+                  self.driver.reply_to(message, msg_txt, file_paths=[filename])
+                  # delete the audio file
+                  self.delete_downloaded_file(filename)
+                  await self.helper.log(f"{message.sender_name} used .drtts")
 
-            except TooManyRequests:
-                self.driver.reply_to(message, "Rate limit exceeded (1/5s)")
+          except TooManyRequests:
+              self.driver.reply_to(message, "Rate limit exceeded (1/5s)")
 
-    async def create_tts_audio(
-        self, text: str, filename: str
-    ) -> Tuple[List, int, float]:
-        loop = asyncio.get_event_loop()
-        engine = pyttsx3.init()
-        await loop.run_in_executor(None, engine.save_to_file, text, filename)
-        await loop.run_in_executor(None, engine.runAndWait)
-        voices = engine.getProperty("voices")
-        rate = engine.getProperty("rate")
-        volume = engine.getProperty("volume")
-        return voices, rate, volume
+  async def create_tts_audio(
+      self, text: str, filename: str
+  ) -> Tuple[List, int, float]:
+      loop = asyncio.get_event_loop()
+      engine = pyttsx3.init()
+      await loop.run_in_executor(None, engine.save_to_file, text, filename)
+      await loop.run_in_executor(None, engine.runAndWait)
+      voices = engine.getProperty("voices")
+      rate = engine.getProperty("rate")
+      volume = engine.getProperty("volume")
+      return voices, rate, volume
 
-    @listen_to(r"^\.tts ([\s\S]*)")
-    async def tts(self, message: Message, text: str):
-        if self.users.is_user(message.sender_name):
-            try:
-                with RateLimit(
-                    resource="drtts",
-                    client=message.sender_name,
-                    max_requests=1,
-                    expire=5,
-                ):
-                    self.helper.add_reaction(message, "speaking_head_in_silhouette")
-                    text = text.replace("\n", " ")
-                    filename = self.create_tmp_filename("mp3")
-                    voices, rate, volume = await self.create_tts_audio(text, filename)
+  @listen_to(r"^\.tts ([\s\S]*)")
+  async def tts(self, message: Message, text: str):
+      if self.users.is_user(message.sender_name):
+          try:
+              with RateLimit(
+                  resource="drtts",
+                  client=message.sender_name,
+                  max_requests=1,
+                  expire=5,
+              ):
+                  self.helper.add_reaction(message, "speaking_head_in_silhouette")
+                  text = text.replace("\n", " ")
+                  filename = self.create_tmp_filename("mp3")
+                  voices, rate, volume = await self.create_tts_audio(text, filename)
 
-                    await self.helper.debug(f"voices: {voices}")
-                    await self.helper.debug(f"rate: {rate}")
-                    await self.helper.debug(f"volume: {volume}")
+                  await self.helper.debug(f"voices: {voices}")
+                  await self.helper.debug(f"rate: {rate}")
+                  await self.helper.debug(f"volume: {volume}")
 
-                    self.driver.reply_to(message, f"tts: {text}", file_paths=[filename])
-                    self.helper.remove_reaction(message, "speaking_head_in_silhouette")
-                    await self.helper.log(f"{message.sender_name} used .tts")
-            except TooManyRequests:
-                self.driver.reply_to(message, "Rate limit exceeded (1/5s)")
+                  self.driver.reply_to(message, f"tts: {text}", file_paths=[filename])
+                  self.helper.remove_reaction(message, "speaking_head_in_silhouette")
+                  await self.helper.log(f"{message.sender_name} used .tts")
+          except TooManyRequests:
+              self.driver.reply_to(message, "Rate limit exceeded (1/5s)")

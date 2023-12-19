a014300509e43687cae293054e65d6e2c567bda6
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-19 22:11:25 +0100

    coder

--- pushups.py
+++ pushups.py
@@ -3,6 +3,7 @@ from mmpy_bot.function import listen_to
 from mmpy_bot.plugins.base import Plugin, PluginManager
 from mmpy_bot.settings import Settings
 from mmpy_bot.wrappers import Message
+import redis
 from common import Helper
 class Pushups(Plugin):
   def __init__(self, log_channel):

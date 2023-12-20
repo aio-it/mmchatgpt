13787ce3040297403797c67e98715d8c29c13c58
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-20 01:43:56 +0100

    coder

---
+++ plugins/base.py
@@ -0,0 +1,23 @@
+
+from mmpy_bot.driver import Driver
+from mmpy_bot.function import listen_to
+from mmpy_bot.plugins.base import Plugin, PluginManager
+from mmpy_bot.settings import Settings
+from mmpy_bot.wrappers import Message
+from plugins.common import Helper
+from plugins.users import Users
+from environs import Env
+class PluginLoader(Plugin):
+    def __init__(self):
+        pass
+    def initialize(self,
+          driver: Driver,
+          plugin_manager: PluginManager,
+          settings: Settings
+          ):
+        self.driver = driver
+        self.settings = settings
+        self.plugin_manager = plugin_manager
+        self.helper = Helper(self.driver)
+        self.users = Users(self.driver, self.plugin_manager, self.settings)
+        self.helper.slog(f"Plugin initialized {self.__class__.__name__}")

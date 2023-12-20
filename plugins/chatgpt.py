f71e75854c92469dc9e0931562b19a1237056ca8
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-20 01:45:26 +0100

    coder

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -398,54 +398,6 @@ class ChatGPT(Plugin):
             except TooManyRequests:
                 self.driver.reply_to(message, "Rate limit exceeded (1/5s)")
 
-    @listen_to(r"^\.redis get ([\s\S]*)")
-    async def redis_get(self, message: Message, key: str):
-        """get redis key"""
-        if self.users.is_admin(message.sender_name):
-            # find the type of the key
-            keytype = self.redis.type(key)
-            if keytype == "string":
-                value = self.redis.get(key)
-            elif keytype == "list":
-                value = self.redis.lrange(key, 0, -1)
-            elif keytype == "set":
-                value = self.redis.smembers(key)
-            elif keytype == "zset":
-                value = self.redis.zrange(key, 0, -1)
-            elif keytype == "hash":
-                value = self.redis.hgetall(key)
-            else:
-                value = "Unknown key type"
-            self.driver.reply_to(message, f"Key: {key}\nValue: {value}")
-
-    @listen_to(r"^\.redis set ([\s\S]*) ([\s\S]*)")
-    async def redis_set(self, message: Message, key: str, value: str):
-        """set redis key"""
-        if self.users.is_admin(message.sender_name):
-            self.redis.set(key, value)
-            self.driver.reply_to(message, f"Key: {key}\nValue: {value}")
-
-    # redis search
-    @listen_to(r"^\.redis search ([\s\S]*)")
-    async def redis_search(self, message: Message, key: str):
-        """search redis key"""
-        if self.users.is_admin(message.sender_name):
-            keys = self.redis.keys(key)
-            keystxt = ""
-            for key in keys:
-                # get the type of the key
-                keytype = self.redis.type(key)
-                keystxt += f" - {key} ({keytype})\n"
-            self.driver.reply_to(message, f"Keys:\n{keystxt}")
-
-    # redis delete
-    @listen_to(r"^\.redis delete ([\s\S]*)")
-    async def redis_delete(self, message: Message, key: str):
-        """delete redis key"""
-        if self.users.is_admin(message.sender_name):
-            self.redis.delete(key)
-            self.driver.reply_to(message, f"Deleted: {key}")
-
     @listen_to(r"^\.set chatgpt ([a-zA-Z0-9_-]+) (.*)")
     async def set_chatgpt(self, message: Message, key: str, value: str):
         """set the chatgpt key"""

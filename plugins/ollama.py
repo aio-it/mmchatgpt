3d55a20dbc7789129f37326827f2c848e7f6ac25
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-21 15:32:58 +0100

    coder

--- plugins/ollama.py
+++ plugins/ollama.py
@@ -38,6 +38,7 @@ class Ollama(PluginLoader):
         if self.redis.get(self.REDIS_PREFIX + "stream_delay") is None:
             self.redis.set(self.REDIS_PREFIX + "stream_delay", self.DEFAULT_STREAM_DELAY)
         self.stream_delay = self.redis.get(self.REDIS_PREFIX + "stream_delay")
+        self.helper.log(f"model: {self.model}")
     @listen_to(r"^\.ollama help")
     async def ollama_help(self, message: Message):
         if self.users.is_admin(message.sender_name):

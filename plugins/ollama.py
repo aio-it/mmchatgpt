add1a9465f951bddf56dbdd9f38f02d0bc5ac1eb
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-21 14:06:33 +0100

    coder

--- plugins/ollama.py
+++ plugins/ollama.py
@@ -239,10 +239,7 @@ class Ollama(PluginLoader):
                                         # update last_update_time
                                         last_update_time = time.time()
 
-                # update the message a final time to make sure we have the full message
-                self.driver.posts.patch_post(
-                    reply_msg_id, {"message": f"{post_prefix}{full_message}"}
-                )
+
             except aiohttp_client_exceptions.ClientConnectorError as error:
                 self.driver.reply_to(message, f"Error: {error}")
                 self.driver.reactions.delete_reaction(
@@ -250,6 +247,10 @@ class Ollama(PluginLoader):
                 )
                 self.driver.react_to(message, "x")
                 return
+        # update the message a final time to make sure we have the full message
+        self.driver.posts.patch_post(
+            reply_msg_id, {"message": f"{post_prefix}{full_message}"}
+        )
         # add response to chatlog
         self.append_chatlog(
             thread_id, {"role": "assistant", "content": full_message}

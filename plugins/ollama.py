94a8e38050b3dc821ec36d8041c1ceda6f4e7a0a
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-21 11:32:33 +0100

    coder

--- plugins/ollama.py
+++ plugins/ollama.py
@@ -73,7 +73,7 @@ class Ollama(PluginLoader):
                                 buffer += char
                                 if '}' in buffer:
                                     try:
-                                        self.log(f"buffer: {buffer}")
+                                        self.helper.log(f"buffer: {buffer}")
                                         obj, idx = json.JSONDecoder().raw_decode(buffer)
                                         buffer = buffer[idx:].lstrip()
                                         if "status" in obj:

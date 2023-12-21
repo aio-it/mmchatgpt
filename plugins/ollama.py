003a8a1a5fdf6dfa8ad9f6b8090863d3393c735c
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-21 17:34:21 +0100

    coder

--- plugins/ollama.py
+++ plugins/ollama.py
@@ -245,7 +245,7 @@ class Ollama(PluginLoader):
             full_message = ""
             post_prefix = f"({self.model}) @{message.sender_name}: "
             # post initial message as a reply and save the message id
-            reply_msg_id = self.driver.reply_to(message, "Working...")["id"]
+            reply_msg_id = self.driver.reply_to(message, f"{self.model} Working...")["id"]
             # send async request to openai
             last_update_time = time.time()
             stream_update_delay_ms = float(100)

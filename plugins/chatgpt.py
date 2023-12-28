e31d0d0cb5dc0f3b31c65509a863d635809a8feb
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-28 05:03:54 +0100

    coder

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -504,7 +504,7 @@ class ChatGPT(PluginLoader):
         response = requests.get(url, headers=self.headers, timeout=10, allow_redirects=True)
         blacklisted_tags = ["script", "style", "meta", "link", "head", "title", "noscript"]
         # debug response
-        await self.helper.debug(f"response: {pformat(response)}")
+        await self.helper.debug(f"response: {pformat(response.text)}")
         try:
             if response.status_code == 200:
                 #extract all text from the webpage

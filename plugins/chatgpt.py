6321aade6c1ab53c7d7ac532bf90c552fe452a73
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2023-12-28 04:53:58 +0100

    coder

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -39,7 +39,7 @@ class MissingApiKey(Exception):
 
 class ChatGPT(PluginLoader):
     """mmypy chatgpt plugin"""
-
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     # MODEL = "gpt-3.5-turbo-0301"
     DEFAULT_MODEL = "gpt-4"
     ALLOWED_MODELS = [

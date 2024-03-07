24af719e404eac5558b30e7db9c0358149278fc9
Lars Bo Rasmussen <lars.bo.rasmussen@sentia.com>
2024-03-08 00:01:23 +0100

    remove old ddgs and debugging

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -511,19 +511,6 @@ class ChatGPT(PluginLoader):
 
                     await self.helper.log(f"{message.sender_name} used .vision")
 
-    async def web_search_4(self, searchterm):
-        """search the web using duckduckgo"""
-        self.exit_after_loop = False
-        from duckduckgo_search import AsyncDDGS
-
-        try:
-            async with AsyncDDGS(headers=self.headers) as ddgs:
-                results = [r async for r in ddgs.text(searchterm, max_results=5)]
-                return results
-        except Exception as e:
-            await self.helper.log(f"Error: {e}")
-            return f"Error: {e}"
-
     async def web_search(self, searchterm):
         """search the web using duckduckgo"""
         self.exit_after_loop = False
@@ -722,9 +709,6 @@ class ChatGPT(PluginLoader):
     async def chat(self, message: Message, model: str = None):
         """listen to everything and respond when mentioned"""
         # set some variables
-        await self.helper.log(
-            f"chat: {message.text} @ {message.sender_name} {self.model} {model}"
-        )
         if model is None:
             model = self.model
         stream = True  # disabled the non-streaming mode for simplicity

848e0a1e50eb0fda2b8afdd3984000d36de57001
Lars Bo Rasmussen <lars.bo.rasmussen@sentia.com>
2024-03-07 23:50:01 +0100

    no idea

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -1065,10 +1065,15 @@ class ChatGPT(PluginLoader):
         """listen to everything and respond when mentioned"""
         await self.chat(message, model="gpt-3.5-turbo")
 
-    @listen_to(r".+", needs_mention=True)
-    @listen_to(r"^@gpt[ \n]+.+", regexp_flag=re_DOTALL)
-    @listen_to(r"^@gpt4[ \n]+.+", regexp_flag=re_DOTALL)
+    @listen_to(r"^@gpt4{0,1}[ \n]+.+", regexp_flag=re_DOTALL)
     async def chat_gpt4(self, message: Message):
+        """listen to everything and respond when mentioned"""
+        if "4" not in self.model:
+            await self.chat(message, "gpt-4-turbo-preview")
+        else:
+            await self.chat(message)
+    @listen_to(r".+", needs_mention=True)
+    async def chat_gpt4_mention(self, message: Message):
         """listen to everything and respond when mentioned"""
         if "4" not in self.model:
             await self.chat(message, "gpt-4-turbo-preview")

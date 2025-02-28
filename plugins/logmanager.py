9c8747bd808f494e02d70e4e17c88dc69620b129
Lars Bo Rasmussen <lrasmussen@aio-it.dk>
2025-02-28 17:19:33 +0000

    sort imports

--- plugins/logmanager.py
+++ plugins/logmanager.py
@@ -1,10 +1,13 @@
-from mmpy_bot.function import listen_to
-from mmpy_bot.wrappers import Message
+import schedule
 from mmpy_bot.driver import Driver
+from mmpy_bot.function import listen_to
 from mmpy_bot.plugins.base import PluginManager
 from mmpy_bot.settings import Settings
+from mmpy_bot.wrappers import Message
+
 from plugins.base import PluginLoader
-import schedule
+
+
 class LogManager(PluginLoader):
     def __init__(self):
         super().__init__()
@@ -36,8 +39,9 @@ class LogManager(PluginLoader):
             if lines_to_keep < 0:
                 lines_to_keep = 0
             # unixtime in ms now - 1 day
-            import time
             import datetime
+            import time
+
             # delete_before = (time.time() * 1000) - (3 * 60 * 1000)
             # delete_before_dt = datetime.datetime.fromtimestamp(delete_before / 1000)
             request_count = 0
@@ -46,7 +50,9 @@ class LogManager(PluginLoader):
             max_requests = int(max_requests)
             delete_immediately = False
             while True:
-                messages = self.driver.posts.get_posts_for_channel(channel_id=self.log_channel, params={"page": page, "per_page": per_page})
+                messages = self.driver.posts.get_posts_for_channel(
+                    channel_id=self.log_channel, params={"page": page, "per_page": per_page}
+                )
                 request_count += 1
                 posts = messages.get("posts", {})
                 post_order = messages.get("order", [])
@@ -83,14 +89,16 @@ class LogManager(PluginLoader):
                 posts_to_delete = list(dict.fromkeys(posts_to_delete))
                 delete_count = len(posts_to_delete)
                 max_log_every_n = 5000
-                log_every_n = min(max_log_every_n,int(delete_count // 10))
+                log_every_n = min(max_log_every_n, int(delete_count // 10))
                 # keep the last 100
                 posts_to_delete = posts_to_delete[:-lines_to_keep]
                 for post_id in posts_to_delete:
                     self.driver.posts.delete_post(post_id)
                     if local_request_count % log_every_n == 0:
                         # log every 500 requests
-                        self.helper.console(f"Deleted {local_request_count}/{delete_count} made {request_count} (max: {max_requests}) requests.")
+                        self.helper.console(
+                            f"Deleted {local_request_count}/{delete_count} made {request_count} (max: {max_requests}) requests."
+                        )
                     request_count += 1
                     local_request_count += 1
             return posts_to_delete, request_count
@@ -99,6 +107,12 @@ class LogManager(PluginLoader):
         if self.users.is_admin(message.sender_name):
             posts_to_delete, request_count = self.purge_logs(lines_to_keep, max_requests)
             if len(posts_to_delete):
-                self.driver.reply_to(message, f"Deleted {len(posts_to_delete)} messages from the log channel {self.log_channel} and made {request_count} (max: {max_requests}) requests.")
+                self.driver.reply_to(
+                    message,
+                    f"Deleted {len(posts_to_delete)} messages from the log channel {self.log_channel} and made {request_count} (max: {max_requests}) requests.",
+                )
             else:
-                self.driver.reply_to(message, f"No messages found in the log channel {self.log_channel} and made {request_count} (max: {max_requests}) requests.")
+                self.driver.reply_to(
+                    message,
+                    f"No messages found in the log channel {self.log_channel} and made {request_count} (max: {max_requests}) requests.",
+                )
